0896014eb876eefa08cd05e7c63f270712443af5
Artorias-8546 <luiporfirio2006@gmail.com>
2025-04-09 17:11:39 -0300

    aaa

--- app/Http/Controllers/AddAnimal.php
+++ app/Http/Controllers/AddAnimal.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\AddAnimal;
 
 class AddAnimal extends Controller
 {
     // camelCase
     function AddAnimal(){
         return view('add-animal');
+    }
+
+    function inserir(Request $request){
+        $animal = new AddAnimal();
+        $animal -> create($request->all());
     }
 }
