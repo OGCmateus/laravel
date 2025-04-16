bbfef764507d81045c963372a6d3df088bf0f87f
Artorias-8546 <luisporfirio2006@gmail.com>
2025-04-16 18:14:38 -0300

    primeiro commit

---
+++ app/Http/Controllers/Personagem.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Personagens;
+
+class Personagem extends Controller
+{
+    function view() {
+        return view('cadastrar-personagem');
+    }
+
+    function salvarPersonagem(Request $dados){
+        //crie o codigo para salvar no bd
+        $personagem = new Personagens();
+        $personagem = $dados->all();
+        $personagem->save();
+    }
+
+    function listarPersonagem() {
+        return view('listar-personagem');
+    }
+}
